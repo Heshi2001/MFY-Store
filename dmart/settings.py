3d70355ec09fabae60c181f765e49c6d25d22fb4
Heshi2001 <euliheshicleetus2001@gmail.com>
2025-06-05 22:25:03 -0700

    Add new field to .env and setting.py

--- dmart/settings.py
+++ dmart/settings.py
@@ -76,8 +76,9 @@ WSGI_APPLICATION = 'dmart.wsgi.application'
 
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600)
+    'default': dj_database_url.parse(config('DATABASE_URL'), conn_max_age=600)
 }
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
